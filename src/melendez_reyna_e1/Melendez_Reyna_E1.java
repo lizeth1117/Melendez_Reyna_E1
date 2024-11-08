98ac10c72515e38e156309746d15b4adba13e562
lizeth1117 <reynamlndz@hotmail.com>
2024-11-08 08:29:19 -0600

    inicio, menu

---
+++ src/melendez_reyna_e1/Melendez_Reyna_E1.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package melendez_reyna_e1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lizet
+ */
+public class Melendez_Reyna_E1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        int opcion ;
+        String menu="1. Piramide"
+                + "2.El Mayor"
+                + "3.Canales"
+                + "4.Caracteres Vocales"
+                + "5. Salir" ;
+        
+        System.out.println(menu);  
+        opcion= entrada.nextInt()   ;
+        
+        if (opcion==1) {
+            
+        }
+         if(opcion==2) {
+             
+         }  
+         if(opcion==3) {
+            
+         }
+         if(opcion==4) {
+             
+         }
+         if(opcion==5) {
+             
+         }
+    } // fin de main
+    
+} // fin de class
